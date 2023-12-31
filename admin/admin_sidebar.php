4e39246876bb8be1d641b4fdb90a6581956d96cc
BuildTools <nasutionrachmat@gmail.com>
2023-12-31 16:26:24 +0700

    move sidebar and top nav to different file

---
+++ admin/admin_sidebar.php
@@ -0,0 +1,34 @@
+<!-- ============================================================== -->
+<!-- Left Sidebar - style you can find in sidebar.scss  -->
+<!-- ============================================================== -->
+<aside class="left-sidebar" data-sidebarbg="skin5">
+  <!-- Sidebar scroll-->
+  <div class="scroll-sidebar">
+    <!-- Sidebar navigation-->
+    <nav class="sidebar-nav">
+      <ul id="sidebarnav" class="pt-4">
+        <li class="sidebar-item">
+          <a class="sidebar-link waves-effect waves-dark sidebar-link" href="test_index.php" aria-expanded="false"><i
+              class="mdi mdi-view-dashboard"></i><span class="hide-menu">Dashboard</span></a>
+        </li>
+        <li class="sidebar-item">
+          <a class="sidebar-link waves-effect waves-dark sidebar-link" href="manage_article.php"
+            aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span class="hide-menu">Manage Articles</span></a>
+        </li>
+        <li class="sidebar-item">
+          <a class="sidebar-link waves-effect waves-dark sidebar-link" href="manage_account.php"
+            aria-expanded="false"><i class="mdi mdi-chart-bubble"></i><span class="hide-menu">Manage Accounts</span></a>
+        </li>
+        <li class="sidebar-item">
+          <a class="sidebar-link waves-effect waves-dark sidebar-link" href="tables.html" aria-expanded="false"><i
+              class="mdi mdi-border-inside"></i><span class="hide-menu">Logs</span></a>
+        </li>
+      </ul>
+    </nav>
+    <!-- End Sidebar navigation -->
+  </div>
+  <!-- End Sidebar scroll-->
+</aside>
+<!-- ============================================================== -->
+<!-- End Left Sidebar - style you can find in sidebar.scss  -->
+<!-- ============================================================== -->
--- admin/manage_article.php
+++ admin/manage_article.php
@@ -157,35 +157,16 @@ $query = $db->get_all_data_article();
     <!-- ============================================================== -->
     <!-- Left Sidebar - style you can find in sidebar.scss  -->
     <!-- ============================================================== -->
-    <aside class="left-sidebar" data-sidebarbg="skin5">
-      <!-- Sidebar scroll-->
-      <div class="scroll-sidebar">
-        <!-- Sidebar navigation-->
-        <nav class="sidebar-nav">
-          <ul id="sidebarnav" class="pt-4">
-            <li class="sidebar-item">
-              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="index.html" aria-expanded="false"><i
-                  class="mdi mdi-view-dashboard"></i><span class="hide-menu">Dashboard</span></a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="manage_article.php"
-                aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span class="hide-menu">Manage
-                  Articles</span></a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="widgets.html" aria-expanded="false"><i
-                  class="mdi mdi-chart-bubble"></i><span class="hide-menu">Manage Accounts</span></a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="tables.html" aria-expanded="false"><i
-                  class="mdi mdi-border-inside"></i><span class="hide-menu">Logs</span></a>
-            </li>
-          </ul>
-        </nav>
-        <!-- End Sidebar navigation -->
-      </div>
-      <!-- End Sidebar scroll-->
-    </aside>
+    <!-- ============================================================== -->
+    <!-- Sidebar and top navbar  -->
+    <!-- ============================================================== -->
+    <?php
+    include("admin_topbar.php");
+    include("admin_sidebar.php");
+    ?>
+    <!-- ============================================================== -->
+    <!-- End of Sidebar and top navbar  -->
+    <!-- ============================================================== -->
     <!-- ============================================================== -->
     <!-- End Left Sidebar - style you can find in sidebar.scss  -->
     <!-- ============================================================== -->
